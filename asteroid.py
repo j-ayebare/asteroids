838959b27d8ae71d3613d384c43c2be08d6793a4
j-ayebare <jeremiahayebare@gmail.com>
2024-10-13 17:46:18 -0400

    All game functionality has been added. Player, asteroids and shots now spawn and are destroyed correctly

---
+++ asteroid.py
@@ -0,0 +1,33 @@
+from circleshape import *
+from constants import *
+import random
+
+white = pygame.Color(255, 255, 255, a=255)
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+     super().__init__(x, y, radius) 
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, white, self.position, self.radius, 2)
+    
+    def update(self, dt):
+       self.move(dt)
+    
+    def move(self, dt):
+       self.position += self.velocity * dt
+
+    def split(self):
+       self.kill()
+       if self.radius <= ASTEROID_MIN_RADIUS:
+          return
+       new_angle = random.uniform(20,50)
+       new_vector1 = self.velocity.rotate(new_angle)
+       new_vector2 = self.velocity.rotate(0 - new_angle)
+       new_radius = self.radius - ASTEROID_MIN_RADIUS
+       new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+       new_asteroid1.velocity = new_vector1 * 1.2
+       new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+       new_asteroid2.velocity = new_vector2 * 1.2
+
+       
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -11,6 +12,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0,0,0)
+    game_time = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
+    new_player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+    enemies = AsteroidField()
+    
 
     while True:
         for event in pygame.event.get():
@@ -18,7 +33,27 @@ def main():
                 return
             
         screen.fill(color)
+
+        for drawn in drawable:
+            drawn.draw(screen)
+
+        for updated in updatable:
+            updated.update(dt)
+        
+        for sprite in asteroids:
+            if sprite.collision(new_player):
+                print ("Game over!")
+                exit()
+        
+        for shot in shots:
+            for sprite in asteroids:
+                if sprite.collision(shot):
+                    shot.kill()
+                    sprite.split()
+                    
+
         pygame.display.flip()
+        dt = game_time.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
